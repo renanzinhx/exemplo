b06bd83b0c72162476d8de38f11a6ecb806900d4
deboracorreia2 <deboracorreia2santos@gmail.com>
2019-04-10 12:19:12 -0300

    Splash scren adicionada

--- app/src/main/java/com/designintercao/splashscreen/MainActivity.kt
+++ app/src/main/java/com/designintercao/splashscreen/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.designintercao.splashscreen
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.support.v4.os.HandlerCompat.postDelayed
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val handle = Handler()
+        handle.postDelayed(Runnable { mostrarLogin() }, 2000)
+    }
+    private fun mostrarLogin() {
+        val intent = Intent(
+            this@MainActivity,
+            Login::class.java
+        )
+        startActivity(intent)
+        finish()
     }
 }
